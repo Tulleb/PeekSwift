0d8941e6fea313cd77c77c6b8a792e0cd006a210
Guillaume Bellut <guillaume@bellut.com>
2022-09-04 12:10:08 +0200

    First commit

---
+++ PeekSwift/PeekSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeekSwiftApp.swift
+//  PeekSwift
+//
+//  Created by Guillaume Bellut on 04/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct PeekSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# PeekSwift
